ad3b6a9ddee2a751ff73ef0f17ee03fab4132924
Ricardo Nainggolan <skatashock@gmail.com>
2019-08-19 16:53:15 +0700

    add personnels filter (still broken)

--- src/components/Personnel/PersonnelList.js
+++ src/components/Personnel/PersonnelList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import PersonnelCard from './PersonnelCard'
+import Form from 'react-bootstrap/Form'
 
 const PERSONNEL_LIST_URL = process.env.REACT_APP_BASE_URL + 'personnel/list/1'
 
@@ -22,8 +23,24 @@ const PersonnelList = () => {
     // eslint-disable-next-line
   }, [])
 
+  const handleFilter = (event) => {
+    let filteredPersonnels = data.personnels
+    filteredPersonnels = filteredPersonnels.filter((item) => {
+      let personnelName = item.name.toLowerCase()
+      return personnelName.indexOf(
+        event.target.value.toLowerCase()) !== -1
+    })
+
+    setData({ personnels: filteredPersonnels, isFetching: false })
+
+    //setData({ personnels: filteredPersonnels, isFetching: false })
+  }
+
   return (
     <React.Fragment>
+      <Form.Control size="lg" type="text" placeholder="Filter employee name" 
+        onChange={handleFilter}
+      />
       <div className="row mb-4 mt-2">
         {data.personnels.map(item => (
           <PersonnelCard key={item.id} {...item} />
